cde5d17a232c2a2fbdf9b72b15dbc5c6c7ce63e6
Kunverse <czkconverse@gmail.com>
2026-08-24 19:00:54 +0800

    nstep

--- 01_tabular_rl/experiments/16_n_step_sarsa.py
+++ 01_tabular_rl/experiments/16_n_step_sarsa.py
@@ -2,14 +2,19 @@ import numpy as np
 from envs.cliff_world import CliffWorld
 from agents.n_step_sarsa_agent import NStepSarsaAgent
 
+# S0 --A0--> S1 --A1--> S2 --A2--> S3--A3--> Terminal
+#      R1         R2         R3        R4
+
+# update, Q(S0​,A0​) --> R1​+γR2​+γ2R3​+γ3Q(S3​,A3​)
 if __name__ == "__main__":
 
+    max_step = 200
     epsilon = 0.2
     gamma = 0.9
     alpha = 0.1
 
     n = 3
-    tao = 0
+    tau = 0
 
     env = CliffWorld()
     state_shape = (env.rows, env.cols)
@@ -34,31 +39,38 @@ if __name__ == "__main__":
 
     for step in range(max_step):
         next_state, new_reward, done = env.step(action)
+
         rewards.append(new_reward)
 
         if not done:
-            next_action = agent.select_action(next_state)
             states.append(next_state)
+            next_action = agent.select_action(next_state)
             actions.append(next_action)
 
-        if done:
-            pass
-
         tau = step - n + 1
-        if tao >= 0:
+        if tau >= 0:
+            if not done:
+                old_state = states[tau]
+                old_action = actions[tau]
+                old_q = agent.q_table[old_state][old_action]
 
-            G = (
-                rewards[-3]
-                + gamma * rewards[-2]
-                + (gamma**2) * rewards[-1]
-                + (gamma**3) * agent.q_table[next_state][next_action]
-            )
+                G = 0
+                for i in range(tau, tau + n):
+                    G = G + gamma ** (i - tau) * rewards[i]
+                G = G + gamma**n * agent.q_table[next_state][next_action]
 
-            old_q = agent.q_table[states[-3]][actions[-3]]
-            agent.q_table[states[-3]][actions[-3]] = old_q + alpha * (G - old_q)
+                new_q = old_q + alpha * (G - old_q)
+                agent.q_table[old_state][old_action] = new_q
+            else:
+                G = 0
+                for i in range(tau, n + 1):
+                    state = states[i]
+                    action = actions[i]
 
-        else:
-            pass
+                    q_old = agent.q_table[state][action]
+
+                    for j in range(0, i):
+                        G = G + gamma * (i)
 
         action = next_action
         state = next_state
