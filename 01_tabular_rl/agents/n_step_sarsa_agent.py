0e37b70037580d90d3b30ac38bf51e3d0d0401f3
Kunverse <czkconverse@gmail.com>
2026-09-01 22:51:12 +0800

    NStepSarsaAgent new frame

--- 01_tabular_rl/agents/n_step_sarsa_agent.py
+++ 01_tabular_rl/agents/n_step_sarsa_agent.py
@@ -1,9 +1,22 @@
 import numpy as np
 
 
+# Answer these questions below:
+# 1 What are T, t, and tau?
+# 2 Why is tau = t - n + 1?
+# 3 If an episode terminates, how do we update the Q table?
+# 4 If an episode is truncated at the fixed max step limit, how do we update the Q table? What is the difference from Question 3?
+# 5 How many update equations exist? What are the update equations in different cases?
 class NStepSarsaAgent:
     def __init__(
-        self, state_shape, num_actions, n=3, alpha=0.1, gamma=0.9, epsilon=0.2
+        self,
+        state_shape,
+        num_actions,
+        n=3,
+        alpha=0.1,
+        gamma=0.9,
+        epsilon=0.2,
+        seed=None,
     ):
         self.n = n
         self.alpha = alpha
@@ -12,6 +25,23 @@ class NStepSarsaAgent:
         self.num_actions = num_actions
 
         self.q_table = np.zeros((*state_shape, num_actions))
+
+        self.rng = np.random.default_rng(seed)
+
+        # buffers of states, actions, rewards
+        self._states = []
+        self._actions = []
+        self._rewards = []
+
+    def begin_episode(self, state):
+        pass
+
+    def learn(
+        self,
+        reward,
+        next_state,
+    ):
+        pass
 
     def select_action(self, state, training=True):
         # epsilon-greedy
